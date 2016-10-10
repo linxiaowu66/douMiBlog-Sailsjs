730c984c4e6d5de9e08ef0a7b5e66298c2b9e444
linxiaowu66 <linguang661990@126.com>
2016-10-10 20:23:33 +0800

    feat(travi): update travi CI script

--- api/models/User.js
+++ api/models/User.js
@@ -4,7 +4,7 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/#!documentation/models
  */
-var bcrypt = require('bcrypt');
+var bcrypt = require('bcryptjs');
 module.exports = {
 
   attributes: {
