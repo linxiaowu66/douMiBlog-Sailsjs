b1b9aef627f53a8ccc8cccc03098a5283f775103
linxiaowu66 <linguang661990@126.com>
2016-11-29 16:15:38 +0800

    rewrite the wrong user name of old committings

---
+++ gitChangeOldUserNameOnCommit.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+git filter-branch --env-filter '
+OLD_EMAIL="linguang@dianwoba.com"
+CORRECT_NAME="linxiaowu66"
+CORRECT_EMAIL="linguang661990@126.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
